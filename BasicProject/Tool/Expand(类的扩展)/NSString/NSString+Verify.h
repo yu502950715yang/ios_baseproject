aed655e58444723e531d0e978fe56af52f85ba6a
余洋 <502950715@qq.com>
2017-10-31 14:03:08 +0800

    正则校验

--- BasicProject/Tool/Expand(类的扩展)/NSString/NSString+Verify.h
+++ BasicProject/Tool/Expand(类的扩展)/NSString/NSString+Verify.h
@@ -10,6 +10,32 @@
 
 @interface NSString (Verify)
 
+/**
+ 邮箱校验
+ */
 - (BOOL)isValidEmail;
 
+/** 手机号码验证 */
+- (BOOL)isValidPhoneNum;
+
+/**
+ IP格式，xxx.xxx.xxx.xxx
+ */
+- (BOOL)isValidIP;
+
+/** 工商税号 */
+- (BOOL)isValidTaxNo;
+
+/** 身份证验证 */
+- (BOOL)isValidIdCardNum;
+
+/**
+ @brief     是否符合最小长度、最长长度，是否包含中文,首字母是否可以为数字
+ @param     minLenth 账号最小长度
+ @param     maxLenth 账号最长长度
+ @param     containChinese 是否包含中文
+ @param     firstCannotBeDigtal 首字母不能为数字
+ @return    正则验证成功返回YES, 否则返回NO
+ */
+- (BOOL)isValidWithMinLenth:(NSInteger)minLenth maxLenth:(NSInteger)maxLenth containChinese:(BOOL)containChinese firstCannotBeDigtal:(BOOL)firstCannotBeDigtal;
 @end
--- BasicProject/VC/ViewController/Validate(校验)/ValidateViewController.m
+++ BasicProject/VC/ViewController/Validate(校验)/ValidateViewController.m
@@ -9,7 +9,14 @@
 #import "ValidateViewController.h"
 #import "NSString+Verify.h"
 
-@interface ValidateViewController ()
+@interface ValidateViewController ()<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *idCardTextField;
+@property (weak, nonatomic) IBOutlet UITextField *IPTextField;
+@property (weak, nonatomic) IBOutlet UITextField *accountNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *taxNoTextField;
 
 @end
 
@@ -17,12 +24,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [_emailTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [_phoneTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [_idCardTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [_IPTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [_accountNumberTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [_taxNoTextField addTarget:self action:@selector(textfieldDidChange:) forControlEvents:UIControlEventEditingChanged];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark 监听textfield输入内容，并对其进行验证
+-(void)textfieldDidChange:(id)sender {
+    UITextField *textField = (UITextField *)sender;
+    NSLog(@"%@", textField.text);
+    switch (textField.tag) {
+        case 1:
+            [self changeTextField:textField valid:[textField.text isValidEmail]];
+            break;
+        case 2:
+            [self changeTextField:textField valid:[textField.text isValidPhoneNum]];
+            break;
+        case 3:
+            [self changeTextField:textField valid:[textField.text isValidIdCardNum]];
+            break;
+        case 4:
+            [self changeTextField:textField valid:[textField.text isValidIP]];
+            break;
+        case 5:
+            [self changeTextField:textField valid:[textField.text isValidWithMinLenth:4 maxLenth:12 containChinese:NO firstCannotBeDigtal:YES]];
+            break;
+        default:
+            [self changeTextField:textField valid:[textField.text isValidTaxNo]];
+            break;
+    }
+}
+
+#pragma mark 改变textfield样式
+-(void)changeTextField:(UITextField *)textField valid:(BOOL)valid {
+    UIColor *color = valid?[UIColor blackColor] : [UIColor redColor];
+    textField.layer.borderColor = color.CGColor;
+    if (valid) {
+        textField.layer.borderWidth = 0;
+    } else {
+        textField.layer.borderWidth = 0.5;
+    }
+}
+
 
 @end
